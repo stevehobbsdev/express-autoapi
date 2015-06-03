fcffaaa8650447cac615106b43e3bf60e7e7da92
Steve Hobbs <elkdanger@googlemail.com>
2015-06-03 13:49:20 +0100

    Added tests for endpoint data

--- spec/integrationSpec.js
+++ spec/integrationSpec.js
@@ -8,17 +8,66 @@ describe('The test integration api', function() {
 
 	var app;
 	var r;
+	var routeData;
+	var endpoints;
+	var routeKeys = ['baseUrl', 'filename', 'routeName'];
 
 	before(function() {
 		
 		app = express();
 
-		api.setup({
+		routeData = api.setup({
 			app: app,
 			source: path.join(__dirname, 'testApi')
 		});
 
+		endpoints = routeData.endpoints;
+
 		r = request(app);
+
+	});
+
+	describe('the endpoint data', function() {
+
+		it('should generate a route for /api', function() {
+			expect(endpoints.index).to.exist;
+			expect(endpoints.index).to.have.all.keys(routeKeys);
+			expect(endpoints.index.baseUrl).to.equal('/api');
+			expect(endpoints.index.filename).to.match(/\\testApi\\index\.js$/);
+			expect(endpoints.index.routeName).to.equal('index');
+		});
+
+		it('should generate a route for /api/user', function() {
+			expect(endpoints.user).to.exist;
+			expect(endpoints.user).to.have.all.keys(routeKeys);
+			expect(endpoints.user.baseUrl).to.equal('/api/user');
+			expect(endpoints.user.filename).to.match(/\\testApi\\user\.js$/);
+			expect(endpoints.user.routeName).to.equal('user');
+		});
+
+		it('should generate a route for /api/sub', function() {
+			expect(endpoints['sub/index']).to.exist;
+			expect(endpoints['sub/index']).to.have.all.keys(routeKeys);
+			expect(endpoints['sub/index'].baseUrl).to.equal('/api/sub');
+			expect(endpoints['sub/index'].filename).to.match(/\\testApi\\sub\\index\.js$/);
+			expect(endpoints['sub/index'].routeName).to.equal('sub/index');
+		});
+
+		it('should generate a route for /api/sub/subs', function() {
+			expect(endpoints['sub/subs']).to.exist;
+			expect(endpoints['sub/subs']).to.have.all.keys(routeKeys);
+			expect(endpoints['sub/subs'].baseUrl).to.equal('/api/sub/subs');
+			expect(endpoints['sub/subs'].filename).to.match(/\\testApi\\sub\\subs\.js$/);
+			expect(endpoints['sub/subs'].routeName).to.equal('sub/subs');
+		});
+
+		it('should generate a route for /api/sub/level3', function() {
+			expect(endpoints['sub/level3/index']).to.exist;
+			expect(endpoints['sub/level3/index']).to.have.all.keys(routeKeys);
+			expect(endpoints['sub/level3/index'].baseUrl).to.equal('/api/sub/level3');
+			expect(endpoints['sub/level3/index'].filename).to.match(/\\testApi\\sub\\level3\\index\.js$/);
+			expect(endpoints['sub/level3/index'].routeName).to.equal('sub/level3/index');
+		});
 
 	});
 
@@ -79,7 +128,7 @@ describe('The test integration api', function() {
 		describe('/level3', function() {
 
 			it('should return a message from the third level', function(done) {
-debugger;
+
 				r.get('/api/sub/level3')
 					.expect({
 						message: 'A message from the third level'
